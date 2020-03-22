bcb603abe891ddbd5c35b9ea17d46bb6b2678723
Sevinj Jafarli <sjafarli@outlook.com>
2020-03-22 18:48:33 +0100

    init2

---
+++ src/main/java/com/utwente/soa/team8MovieProject/Team8MovieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.utwente.soa.team8MovieProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team8MovieProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team8MovieProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utwente/soa/team8MovieProject/Team8MovieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utwente.soa.team8MovieProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team8MovieProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
